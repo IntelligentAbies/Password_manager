c02b4d8642941923be872697208648b474a6c058
IntelligentAbies <intelligentabies@gmail.com>
2025-05-15 21:00:54 +0200

    13/05

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import Controller.Controller;
-import View.MainPanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Password Manager by Diego Ferventi");
+        try {
+            //Sta roba serve solo per fare in modo che le immagini poi vengono caricate nel binario
+            InputStream is = Main.class.getResourceAsStream("/shield.png");
+            ImageIcon logo = new ImageIcon(ImageIO.read(is));
+            Image scaledLogo = logo.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+            frame.setIconImage(scaledLogo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Controller controller = new Controller();
         frame.getContentPane().add(controller.getMainPanel());
